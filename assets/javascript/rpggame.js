54e627d4df185fba6e19e5153a47b8722de3296c
Astrid <leigh.stevens.ls@gmail.com>
2019-02-18 20:55:08 -0600

    changed objects to an array

--- assets/javascript/rpggame.js
+++ assets/javascript/rpggame.js
@@ -1,160 +1,136 @@
-$( document ).ready(function() 
+$(function() {
 
-//contain all options of characters in an object - connect the DOM element with each nested object
-{var allCharacters = {
-    han = {
+
+//hide reset button on page load
+    $("#fade").fadeIn("slow");
+
+
+//contain all options of characters in an array
+var allCharacters = [
+    {  id: 1,
+       name: Han,
        img: "assets/images/Han_Solo.jpg",
        health_points: 120,
        attack_loss: 5,
        attack_points: 10,
        attacked_text: "Han Solo lost 5 health points.",
-       attack_text: "Han Solo attacked for 10 points."
+       attack_text: "Han Solo attacked for 10 points.",
+       attacker: false
 
     },
 
-    yoda = {
+    {   id: 2,
+        name: yoda,
         img: "assets/images/Yoda.jpg",
         health_points: 100,
         attack_loss: 10,
         attack_points: 15,
         attacked_text: "Yoda lost 10 health points.",
         attack_text: "Yoda attacked for 15 points.",
+        attacker: false
 
      },
 
-     darthV = {
+    {   id: 3,
+        name: darthV,
         img: "assets/images/Darth_Vader.jpg",
         health_points: 80,
         attack_loss: 20,
         attack_points: 5,
         attacked_text: "Darth Vader lost 5 health points.",
         attack_text: "Darth Vader attacked for 10 points.",
+        attacker: false
 
      },
 
-     bobbaF = {
-        img: "assets/images/Boba Fett.jpg",
+     {  id: 4,
+        name: bobbaF,
+        img: "assets/images/Boba_Fett.jpg",
         health_points: 70,
         attack_loss: 15,
         attack_points: 10,
         attacked_text: "Bobba Fett lost 15 health points.",
-        attack_text: "Bobba Fett attacked for 10 points."
+        attack_text: "Bobba Fett attacked for 10 points.",
+        attacker: false
 
-     },
+     }
+]
 
+
+var chosenCharacter
+
+
+function displayInitalCharacters(){
+    $('#characters').empty()
+    for (let i = 0; i < characters.length; i++) {
+        var newImage = $('<img>')
+        newImage.attr('src', characters[i].image)
+        newImage.attr('data-name', characters[i].name)
+        newImage.addClass('each-characters')
+        $('#characters').append(newImage)
+    }
 }
 
-//declare a variable to collect all characters into one variable
-var allCharacters = han, yoda, darthV, bobbaF
-var character = allCharacters
+displayInitalCharacters()
 
-//click events for each image div for the user to select their attack player
-//changes selected to true and moves player to attack player position
-    function initializeGame () {
 
-    $("#hImg").click(function(selected) {
-        selected = true;
-        $(function() {
-            $("#hImg").css("margin-left");
-            $("#hImg").css("margin-left: 150px");
-        })
+function displayChosenCharacter(){
+    var newImage = $('<img>')
+    newImage.attr('src', chosenCharacter.image)
+    // newImage.attr('data-id', [i])
+    // newImage.addClass('each-characters')
+    $('#chosen-character').append(newImage)
+}
+
+function displayDefendingCharacter(){
+    var newImage = $('<img>')
+    newImage.attr('src', chosenCharacter.image)
+    // newImage.attr('data-id', [i])
+    // newImage.addClass('each-characters')
+    $('#defending-character').append(newImage)
+}
+
+
+$(document).on('click', '.each-characters', function(){
+    var name = $(this).attr('data-name')
+    console.log(name);   
+     
+    // Use for loop to remove character from initl array
+     var index = characters.findIndex(name)
+    
+    if(!chosenCharacter){
+        chosenCharacter = characters.splice(index, 1)[0]
+        displayChosenCharacter()
+    } else {
+        defendingCharacter = characters.splice(index, 1)[0]
+        displayDefendingCharacter()
     }
-        );
 
-    $("#yImg").click(function(selected) {
-        selected = true;
-        $(function() {
-            $("#yImg").css("margin-left");
-            $("#yImg").css("margin-left: 20px");
-    }
-        );
-
-    $("#dImg").click(function(selected) {
-        selected = true;
-        $(function() {
-            $("#dImg").css("margin-left");
-            $("#dImg").css("margin-left: -50px");
-    }
-        );
-
-    $("#bImg").click(function(selected) {
-        selected = true;
-        $(function() {
-            $("#bImg").css("margin-left");
-            $("#bImg").css("margin-left: -100px");
-    }
-        );
-
-//click events for each image div for the user to select their enemy to attack
-//changes selected to true and moves player to attack player position
-function chooseEnemy () {
-
-    $("#hImg").click(function(selected) {
-        selected = true;
-        $(function() {
-            $("#hImg").css("margin-left");
-            $("#hImg").css("margin-left: 350px");
-    }
-        );
-
-    $("#yImg").click(function(selected) {
-        selected = true;
-        $(function() {
-            $("#yImg").css("margin-left");
-            $("#yImg").css("margin-left: 150px");
-    }
-        );
-
-    $("#dImg").click(function(selected) {
-        selected = true;
-        $(function() {
-            $("#dImg").css("margin-left");
-            $("#dImg").css("margin-left: 150px");
-    }
-        );
-
-    $("#bImg").click(function(selected) {
-        selected = true;
-        $(function() {
-            $("#bImg").css("margin-left");
-            $("#bImg").css("margin-left: 150px");
-    }
-        );
-
-
-//call functions to start the game
-startDefaults()
-
-initializeGame()
-
-selectCharacter()
-
-chooseEnemy()
-//loop through the object to select player for playing based on on-click event
-
-
+    displayInitalCharacters()
+    
+})
 
 
 /*declare function for attacks:  on clicking the attack button, for the selected character,
 if the selected character=true, update healthpoints, write the new healthpoint number to the UI,
 and show the attack text for that specific character*/
 function attack () {
-    if ($("#attackButton").on("click", function() {
-        for (this.character in allCharacters){
-            if (character.selected(true));
-                character.new_health_points = (character.health_points - character.attack_loss)
-                $(".HP").text(character.new_health_points);
-                $("attacktext").text(character.attack_text);
+    if ($("#attackButton").click(function() {
+        for (this.chosenCharacter in allCharacters){
+            if (chosenCharacter.selected(true));
+                characterNew_health_points = (chosenCharacter.health_points - chosenCharacter.attack_loss)
+                $(".HP").text(chosenCharacterNew_health_points);
+                $("attacktext").text(chosenCharacter.attack_text);
                 checkScore();
             }
 
 
 //declare function for checking the score each time
 function checkScore() {
-if (this.character.new_health_points >= 0) {
+if (this.characterNew_health_points >= 0) {
     playGame()}
 
-if (this.character.new_health_points >=0), (this.enemy.new_health_points <= 0) {
+if (this.characterNew_health_points > 0 && this.enemyNew_health_points <= 0) {
     win = true;
     winsVsLosses();}
 
@@ -193,3 +169,5 @@ function reset(); {
 initializeGame();
 
 selectCharacter();
+
+    }
